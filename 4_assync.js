7fefad220ecffdf93058ce971f5ae2b2e3781f37
Oleh <66703682+OlehBabiak@users.noreply.github.com>
2020-10-12 19:22:20 +0300

    Update 4_assync.js

--- 4_assync.js
+++ 4_assync.js
@@ -1,55 +1,74 @@
 console.log('Request data..')
+// //
+// // setTimeout(() => {
+// // 	console.log('Preparing data..')
+// //
+// // 	const backendData ={
+// // 		server: 'aws',
+// // 		port: 2000,
+// // 		status: 'working'
+// // 	}
+// // 	setTimeout(() => {
+// // 		backendData.modified = true
+// // 		console.log('Data received', backendData)
+// // 	}, 2000)
+// // }, 2000)
 //
-// setTimeout(() => {
+// const p = new Promise((resolve, reject) => {
+// setTimeout(() =>{
 // 	console.log('Preparing data..')
-//
-// 	const backendData ={
+// 	const backendData = {
 // 		server: 'aws',
 // 		port: 2000,
 // 		status: 'working'
 // 	}
-// 	setTimeout(() => {
-// 		backendData.modified = true
-// 		console.log('Data received', backendData)
-// 	}, 2000)
+// 	resolve(backendData)
 // }, 2000)
-
-const p = new Promise((resolve, reject) => {
-setTimeout(() =>{
-	console.log('Preparing data..')
-	const backendData = {
-		server: 'aws',
-		port: 2000,
-		status: 'working'
-	}
-	resolve(backendData)
-}, 2000)
-})
-
+// })
+//
+// // p.then(data => {
+// // 	const p2 = new Promise((resolve, reject) => {
+// // 		setTimeout(() => {
+// // 			data.modified = true
+// // 			resolve(data)
+// // 		}, 2000)
+// // 	})
+// // 	p2.then(clientData => {
+// // 		console.log('Data received', clientData)
+// // 	})
+// // })
+//
+// // but better:
+//
 // p.then(data => {
-// 	const p2 = new Promise((resolve, reject) => {
+// 	return  new Promise((resolve, reject) => {
 // 		setTimeout(() => {
 // 			data.modified = true
-// 			resolve(data)
+// 			reject(data)
 // 		}, 2000)
 // 	})
-// 	p2.then(clientData => {
-// 		console.log('Data received', clientData)
 // 	})
+// 	.then(clientData => {
+// 	clientData.fromPromise = true
+// 	return clientData
+// }).then(data => {
+// 	console.log('Modified', data)
 // })
+// 	.catch(err => console.error('Error:', err))
+// .finally(() => console.log('Finally'))
 
-// but better:
+const  sleep = ms => {
+	return new Promise(resolve =>
+	setTimeout(() => resolve(), ms)
+	)
+}
+// sleep(2000).then(() => console.log('After 2 sec'))
+// sleep(5000).then(() => console.log('After 5 sec'))
 
-p.then(data => {
-	return  new Promise((resolve, reject) => {
-		setTimeout(() => {
-			data.modified = true
-			resolve(data)
-		}, 2000)
-	})
-	}).then(clientData => {
-	clientData.fromPromise = true
-	return clientData
-}).then(data => {
-	console.log('Modified', data)
-})
+Promise.all([sleep(2000), sleep(5000)]).then(() =>
+console.log('All promises')
+)
+
+Promise.race([sleep(2000), sleep(5000)]).then(() =>
+	console.log('Race promises')
+)
