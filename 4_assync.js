06a49c769143d1ac171d8e0ed5becf674c3ec80a
Oleh <66703682+OlehBabiak@users.noreply.github.com>
2020-10-12 18:51:17 +0300

    Add code to promise file

--- 4_assync.js
+++ 4_assync.js
@@ -1,5 +1,52 @@
-console.log('Start')
-console.log('Start2')
-setTimeout(function () {
-	console.log('Insidw timeout after 2 sec')
+console.log('Request data..')
+//
+// setTimeout(() => {
+// 	console.log('Preparing data..')
+//
+// 	const backendData ={
+// 		server: 'aws',
+// 		port: 2000,
+// 		status: 'working'
+// 	}
+// 	setTimeout(() => {
+// 		backendData.modified = true
+// 		console.log('Data received', backendData)
+// 	}, 2000)
+// }, 2000)
+
+const p = new Promise((resolve, reject) => {
+setTimeout(() =>{
+	console.log('Preparing data..')
+	const backendData = {
+		server: 'aws',
+		port: 2000,
+		status: 'working'
+	}
+	resolve(backendData)
 }, 2000)
+})
+
+// p.then(data => {
+// 	const p2 = new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 			data.modified = true
+// 			resolve(data)
+// 		}, 2000)
+// 	})
+// 	p2.then(clientData => {
+// 		console.log('Data received', clientData)
+// 	})
+// })
+
+// but better:
+
+p.then(data => {
+	return  new Promise((resolve, reject) => {
+		setTimeout(() => {
+			data.modified = true
+			resolve(data)
+		}, 2000)
+	})
+	}).then(clientData => {
+	console.log('Data received', clientData)
+})
