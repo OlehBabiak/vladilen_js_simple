7887f1bc212bb7261c715f0480f0291a508c1fd2
Oleh <66703682+OlehBabiak@users.noreply.github.com>
2020-10-15 22:53:14 +0300

    Add file create.object

--- object.create.js
+++ object.create.js
@@ -1,1 +1,38 @@
+const person = Object.create({
+    calculateAge(){ // метод додається в прототип
+        console.log('Age:', new Date().getFullYear() - this.birthYear)
+    }
+}, {
+    name: {
+        value: 'Oleh',
+        enumerable: true, // property descriptor який відповідає за видимість ключів при ітеріції в циклі
+        writable: true, // property descriptor який відповідає за можливість зміни значень ключів
+        configurable: true // property descriptor який відповідає за можливість видалення ключів
+    },
+    birthYear: {
+        value: 1981,
+        enumerable: false,
+        writable: false,
+        configurable: false
+    },
+    age: {
+        get() {
+            return new Date().getFullYear() - this.birthYear
+        },
+        set(value) {
+            document.body.style.background = 'red'
+            console.log('Set age:', value)
+        }
+    }
+})
+
+// person.name = 'Maxim'
+
+for (key in person){
+    console.log(
+        is(person.hasOwnProperty(key)){  // умова відображає власні ключіобєкта, не враховує прототипів
+        console.log('Key', key, person[key])
+    }
+    )
+}
 
